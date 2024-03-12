939ec0344811eb5b49f838437fb564d2dd73da93
Igor Nikiforov <inikiforovj7@icloud.com>
2024-03-12 11:03:12 +0500

    Initial commit

---
+++ CalendarApp/CalendarAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarAppApp.swift
+//  CalendarApp
+//
+//  Created by Игорь Никифоров on 12.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct CalendarAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
